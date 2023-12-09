7cbc314fdce6ade01f83f76651cdcbf3d0056c19
kkaizer11 <kaizerin.m@gmail.com>
2023-12-09 16:45:13 -0300

    commit do sabádo

---
+++ Js/5 - Arrays/1-arrays.js
@@ -0,0 +1,12 @@
+// ?
+
+// don't touch below this line
+
+function logArray(arr) {
+  console.log('logging array...')
+  for (const a of arr) {
+    console.log(` - ${a}`)
+  }
+  console.log('---')
+}
+
